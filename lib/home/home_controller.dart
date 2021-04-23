5dd525cc5163ef68d6173b6a1448b2e680f4838c
Rafa menegon <dev.rafaelmenegon@gmail.com>
2021-04-22 22:47:57 -0300

    Day 3: Home repository creation

--- lib/home/home_controller.dart
+++ lib/home/home_controller.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_devquiz/core/app_images.dart';
+import 'package:flutter_devquiz/home/home_repository.dart';
 import 'package:flutter_devquiz/home/home_state.dart';
 import 'package:flutter_devquiz/models/answer_model.dart';
 import 'package:flutter_devquiz/models/question_model.dart';
 import 'package:flutter_devquiz/models/quiz_model.dart';
 import 'package:flutter_devquiz/models/user_model.dart';
 import 'package:flutter/foundation.dart';
-
 
 class HomeController {
   final stateNotifier = ValueNotifier<HomeState>(HomeState.empty);
@@ -16,47 +16,17 @@ class HomeController {
   UserModel? user;
   List<QuizModel>? quizzes;
 
+  final repository = HomeRepository();
+
   void getUser() async {
     state = HomeState.loading;
-    await Future.delayed(Duration(seconds: 2));
-    user = UserModel(name: "Rafael Menegon", photoUrl: "https://avatars.githubusercontent.com/u/65569815?v=4");
+    user = await repository.getUser();
     state = HomeState.success;
   }
 
   void getQuizzes() async {
     state = HomeState.loading;
-    await Future.delayed(Duration(seconds: 2));
-    quizzes = [
-      QuizModel(
-          title: "NLW 5 - Flutter",
-          image: AppImages.blocks,
-          questionAnswered: 2,
-          level: Level.facil,
-          questions: [
-            QuestionModel(title: "Está curtindo Flutter?", answers: [
-              AnswerModel(title: "Estou curtindo"),
-              AnswerModel(title: "Está legal"),
-              AnswerModel(title: "Está bacana"),
-              AnswerModel(title: "Está supimpa", isRight: true),
-            ],
-            ),
-            QuestionModel(title: "Está curtindo Flutter?", answers: [
-              AnswerModel(title: "Estou curtindo"),
-              AnswerModel(title: "Está legal"),
-              AnswerModel(title: "Está bacana"),
-              AnswerModel(title: "Está supimpa", isRight: true),
-            ],
-            ),
-            QuestionModel(title: "Está curtindo Flutter?", answers: [
-              AnswerModel(title: "Estou curtindo"),
-              AnswerModel(title: "Está legal"),
-              AnswerModel(title: "Está bacana"),
-              AnswerModel(title: "Está supimpa", isRight: true),
-            ],
-            ),
-          ],
-      )
-    ];
+    quizzes = await repository.getQuizzes();
     state = HomeState.success;
   }
 }
